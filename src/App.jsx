a957f355d7e48ce0812381954f6bdf921b0557ca
Guilherme Freire <guitenan@hotmail.com>
2024-05-13 10:07:17 -0300

    Criação até data do post e comentarios

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from './App.module.css';
+
+import './global.css';
+
+const posts = [ 
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/guitefreire.png',
+      name: 'Guilherme Freire',
+      role: 'Intern @ Petrobrás'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' }, 
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' }, 
+      { type: 'link', content: '👉 gui.design/doctorcare'},
+    ],
+
+    publishedAt: new Date('2024-05-12 16:00:00'),
+  },
+
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/MariaIsabel68.png',
+      name: 'Maria Isabel',
+      role: 'Software Engineer @ Passei Direto'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' }, 
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' }, 
+      { type: 'link', content: '👉 mary.design/doctorcare'},
+    ],
+
+    publishedAt: new Date('2024-05-12 16:00:00'),
+  }
+];
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map(post => {
+            return (
+            <Post
+              author={post.author}
+              name={post.author.name}
+              role={post.author.role}
+              publishedAt={post.publishedAt}
+              content={post.content}            
+            />
+            )
+          })}
+        </main>
+        </div>    
+    </div>
+  )
+}
---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from '@phosphor-icons/react';
+import styles from './Comment.module.css';
+import { Avatar } from './Avatar';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/guitefreire.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Guilherme Freire</strong>
+                            <time title="10 de maio de 2024 às 16:38" dateTime="2024-05-10 16:38:00">Cerca de 1h</time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+
+                    <p>Muito bom, parabéns!</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
